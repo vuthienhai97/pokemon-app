4c458f727e1ce41ca2818d7df6381e02c3a7acbc
vuthienhai97gh <vuthienhai97@gmail.com>
2026-04-30 14:56:38 +0700

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-        pathname: "/PokeAPI/sprites/**",
-      },
-    ],
-  },
-};
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '/PokeAPI/sprites/**'
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
